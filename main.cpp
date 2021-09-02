b669b3a2a606b92bfb1adb01cea42b0995e247f8
Bernar-duarte <60206917+Bernar-duarte@users.noreply.github.com>
2021-09-02 20:57:12 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+    int num;
+
+    cout <<"Sentado na taberna da guilda em mais um dia frio do rigoroso inverno de Freljord, sem dinheiro para sobreviver, o implacável mago vai até o mural de missões e se depara com missões de diferentes níveis. Uma fácil que não lhe retornaria muito dinheiro, outra mediana que poderia lhe dar alguns dias de folga e uma a desafiar suas habilidades místicas, porém iria lhe recompensar pela tamanha dificuldade.     ";
+    cout<<"\ndigite 1 para escolher a missão facil, 2 para a media e 3 para a missão dificil.";
+    cin>>num;
+
+    if (num = 1 ){
+        cout<<"missao facil";
+    }
+    if (num == 2){
+        cout<<"missao media";
+    }
+    if (num == 3){
+        cout<<" Apos escolher a missao mais dificil voce vira o contrato onde teria informacoes sobre o resgate.\n                    Presa em Ascalack .\n Ascalack, e a ilha mais distante de todo o reino, alem de ser uma viagem longa e rigorosa com dificuldades pelo caminho, a princesa era guardada por um antigo Mago Ascalackiano (Ascalackianos eram os magos mais experientes do reino)\n Todos os magos do reino têm um ponto fraco e esta é a única maneira de derrotar um mago mais experiente.\nPara descobrir o ponto fraco de um mago, basta fazer um feitiço que tem os seguintes requisitos para ser feito: \nNome do Mago \nOlho de Mercus \nSer feito pela Bruxa Suprema \nSer feito Próximo ao Mago    ";
+        if( num =1){
+            cout<<"fsdjfsidlf";
+        }
+        if(num =2){
+            cout<<"morra";
+
+        }
+    }
+
+    return 0;
+}
