056d2ac077476770dc519ad93e037a5d300ddc51
Bernar-duarte <60206917+Bernar-duarte@users.noreply.github.com>
2021-09-02 23:10:54 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,36 +10,36 @@ int main()
     cin>>num;
 
 
-   //InÃ­cio histÃ³ria 1:
+   //Início história 1:
     if (num == 1){
         cout<<"missao facil";
     }
 
-    //InÃ­cio histÃ³ria 2:
+    //Início história 2:
     if (num == 2){
-        cout<<"missao merda";
+        cout<<"missao SSS";
     }
 
-    //Inicio histÃ³ria 3:
+    //Inicio história 3:
     if (num == 3){
-        cout<<"ApÃ³s escolher a missÃ£o para resgatar a princesa, vocÃª lÃª: 'Presa em Ascalack' ,nas informaÃ§Ãµes do contrato.";
-        cout<<"/nAscalack, Ã© a ilha mais distante de todo o reino, alÃ©m de ser uma viagem longa e rigorosa com dificuldades pelo caminho, a princesa era guardada por um antigo Mago Ascalackiano";
+        cout<<"Após escolher a missão para resgatar a princesa, você lê: 'Presa em Ascalack' ,nas informações do contrato.";
+        cout<<"/nAscalack, é a ilha mais distante de todo o reino, além de ser uma viagem longa e rigorosa com dificuldades pelo caminho, a princesa era guardada por um antigo Mago Ascalackiano";
         cout<<"\n(Ascalackianos eram os magos mais experientes do reino).";
-        cout<<"Todos os magos do reino tÃªm um ponto fraco e esta Ã© a Ãºnica maneira de derrotar um mago mais experiente.";
-        cout<<"Para descobrir o ponto fraco de um mago, basta fazer um feitiÃ§o que tem os seguintes requisitos para ser feito:";
-        //REQUISITOS PARA FAZER O FEITIÃ‡O:
-        cout<<"\n1-Nome do Mago\n2-Olho de Mercus (Material mais poderoso do reino)\n3-Ser feito pela Bruxa Suprema\n4-Ser feito PrÃ³ximo ao Mago";
-        cout<<"\nEscolha o nÃºmero do requisito o qual vocÃª deseja usar para fazer o feitiÃ§o";
+        cout<<"Todos os magos do reino têm um ponto fraco e esta é a única maneira de derrotar um mago mais experiente.";
+        cout<<"Para descobrir o ponto fraco de um mago, basta fazer um feitiço que tem os seguintes requisitos para ser feito:";
+        //REQUISITOS PARA FAZER O FEITIÇO:
+        cout<<"\n1-Nome do Mago\n2-Olho de Mercus (Material mais poderoso do reino)\n3-Ser feito pela Bruxa Suprema\n4-Ser feito Próximo ao Mago";
+        cout<<"\nEscolha o número do requisito o qual você deseja usar para fazer o feitiço";
         cin>>num;
         //NOME
         if(num==1){
-            cout<<"\nO nome de um mago Ã© algo secreto muito secreto";
-            cout<<"\nPorÃ©m os nomes e as posiÃ§Ãµes de todos os habitantes do reino estÃ£o escritos no livro do espaÃ§o/tempo, que Ã© guardado por Osceles";
-            cout<<"\n(um veterano de batalha que possui muita forÃ§a fÃ­sica)";
-            cout<<"\nCom isso em mente, vocÃª vai atÃ© Bulfrock, o dono da Guilda, para perguntar sobre o livro.";
-            cout<<"\n-Onde fica o livro do espaÃ§o tempo Bulfrock?";
+            cout<<"\nO nome de um mago é algo secreto muito secreto";
+            cout<<"\nPorém os nomes e as posições de todos os habitantes do reino estão escritos no livro do espaço/tempo, que é guardado por Osceles";
+            cout<<"\n(um veterano de batalha que possui muita força física)";
+            cout<<"\nCom isso em mente, você vai até Bulfrock, o dono da Guilda, para perguntar sobre o livro.";
+            cout<<"\n-Onde fica o livro do espaço tempo Bulfrock?";
             cout<<"\nEspantado, Bulfrock responde:";
-            cout<<"\nHÃ¡ um veterano de batalha neste local, sair de lÃ¡ com o livro serÃ¡ um trabalho difÃ­cil, mas posso lhe dar um mapa com as coordenadas para o local";
+            cout<<"\nHá um veterano de batalha neste local, sair de lá com o livro será um trabalho difícil, mas posso lhe dar um mapa com as coordenadas para o local";
             cout<<"digite num =2";
             cin>>num;
          }
@@ -51,9 +51,9 @@ int main()
         if(num==3){
             cout<<"Ser Feito Pela Bruxa Suprema";
         }
-        //SER FEITO PRÃ“XIMO AO MAGO
+        //SER FEITO PRÓXIMO AO MAGO
         if(num==4){
-            cout<<"Ser Feito PrÃ³ximo Ao Mago";
+            cout<<"Ser Feito Próximo Ao Mago";
         }
 
     }
